33d9af73c86ef718952abfabcdcdcac4caf5bccc
liftctrl <yangxianzhan@gmail.com>
2025-11-29 17:33:34 +0800

    fix(nvim/options): correct configuration errors in options file

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,34 +1,33 @@
 --- Line numbers
-vim.opt.number = true        -- show absolute line numbers
-vim.opt.relativenumber = true -- show relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- Search settings
-vim.opt.hlsearch = true       -- highlight search results
-vim.opt.incsearch = true      -- incremental search as you type
-vim.opt.ignorecase = true     -- case-insensitive search
-vim.opt.smartcase = true      -- but case-sensitive if uppercase letters used
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Line wrapping
-vim.opt.wrap = false          -- disable line wrap
-vim.opt.linebreak = true      -- break at word boundaries if wrap enabled
+vim.opt.wrap = false
+vim.opt.linebreak = true
 
 -- Tabs and Indentation
-vim.opt.expandtab = true      -- use spaces instead of tabs
-vim.opt.tabstop = 4           -- number of spaces per tab
-vim.opt.shiftwidth = 4        -- spaces used for autoindent
-vim.opt.softtabstop = 4       -- editing behaves like 4 spaces
-vim.opt.autoindent = true     -- copy indent from previous line
-vim.opt.smartindent = true    -- smart autoindent
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- Appearance
-vim.opt.signcolumn = "yes"     -- always show sign column
-vim.opt.showmode = "yes"        -- hide --INSERT-- text
-vim.opt.showcmd = true          -- show incomplete command in bottom
+vim.opt.showmode = true
+vim.opt.showcmd = true
 
 -- Performance and Misc
-vim.opt.updatetime = 300       -- faster completion and CursorHold events
-vim.opt.timeoutlen = 500       -- key sequence timeout
-vim.opt.lazyredraw = true      -- speed up macros and scrolling
-vim.opt.swapfile = false       -- disable swap file
-vim.opt.backup = false         -- disable backup file
-vim.opt.writebackup = false    -- disable write backup
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.lazyredraw = true
+vim.opt.swapfile = false
+vim.opt.backup = false 
+vim.opt.writebackup = false 
