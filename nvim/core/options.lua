ca10c7914aa27127889e1631b359602bc1d2d1cf
liftctrl <yangxianzhan@gmail.com>
2026-02-28 23:47:46 +0800

    chore: delete lazy.lua and options.lua, and add nvim/core directory

---
+++ nvim/core/options.lua
@@ -0,0 +1,33 @@
+--- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Search settings
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Line wrapping
+vim.opt.wrap = false
+vim.opt.linebreak = true
+
+-- Tabs and Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Appearance
+vim.opt.showmode = true
+vim.opt.showcmd = true
+
+-- Performance and Misc
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.lazyredraw = true
+vim.opt.swapfile = false
+vim.opt.backup = false 
+vim.opt.writebackup = false 
